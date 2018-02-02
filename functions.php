b4d80f5219c1eba3517e70a0eb7af93be84c6988
Alvin Grant <alving.nyc@gmail.com>
2018-02-02 09:11:03 -0500

    Create plugin/shortcode to display recent posts
    Register shortcodes and recent-posts plugin

--- functions.php
+++ functions.php
@@ -1,3 +1,11 @@
 <?php
 
 add_filter( 'x_enqueue_parent_stylesheet', '__return_true' );
+
+include(dirname(__FILE__) . '/plugins/recent-posts/recent-posts.php');
+
+// Register Shortcodes
+add_action( 'init', 'register_shortcodes');
+function register_shortcodes() {
+	add_shortcode('recent-posts', 'recent_posts');
+}
