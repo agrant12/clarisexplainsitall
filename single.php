9cec62bd3794c21425314b7b189dbc29d7aa4b16
Alvin Grant <alving.nyc@gmail.com>
2018-03-24 18:17:25 -0400

    Update metadata display

--- single.php
+++ single.php
@@ -18,27 +18,27 @@ $fullwidth = get_post_meta( get_the_ID(), '_x_post_layout', true );
 					<?php while ( have_posts() ) : the_post(); ?>
 						<div class="entry-wrap">
 							<div class="metadata">
-								<div class="dates"><?php the_date(); ?></div>
+								<div class="dates"><?php esc_html(the_date()); ?></div>
 								<div class="author">| by <?php the_author(); ?></div>
-								<div class="comments">| <?php echo comments_number(); ?></div>
+								<div class="comments">| <?php echo esc_html(comments_number()); ?></div>
 								<div class="categories">|
 									<?php $categories = get_the_category();
 										$count = count($categories);
 										foreach ($categories as $key => $category): ?>
 											<?php if ($key === ($count - 1)): ?>
 												<a href="<?php echo home_url("/category/" . $category->slug); ?>">
-													<?php echo $category->name; ?>
+													<?php echo esc_html($category->name); ?>
 												</a>
 											<?php else: ?>
 												<a href="<?php echo home_url("/category/" . $category->slug); ?>">
-													<?php echo $category->name . ","; ?>
+													<?php echo esc_html($category->name) . ","; ?>
 												</a>
 											<?php endif; ?>
 										<?php endforeach; 
 									?>
 								</div>
 							</div>
-							<h1 class="entry-title"><?php the_title(); ?></h1>
+							<h1 class="entry-title"><?php esc_html(the_title()); ?></h1>
 						</div>
 						<div class="entry-featured"><div class="entry-thumb"><?php the_post_thumbnail('full'); ?></div></div>
 
